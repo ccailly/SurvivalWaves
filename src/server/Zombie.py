d257f8ab1ff6602bef93dfc130cc982cfc38d6cb
Alexandre ANGOT <alexandre.angot21@orange.fr>
2023-11-14 23:20:00 +0100

    ✨: Add methods to Zombie class

--- src/server/Zombie.py
+++ src/server/Zombie.py
@@ -1,6 +1,8 @@
 import j2l.pytactx.agent as pytactx
+from random import randint
 
 class Zombie (pytactx.Agent):
+    
     def __init__(self, name):
         super().__init__(
             playerId=name,
@@ -8,4 +10,17 @@ class Zombie (pytactx.Agent):
             username="demo", 
             password="demo", 
             server="mqtt.jusdeliens.com", 
-            verbosity=2) 
+            verbosity=2
+        )
+
+    def wander(self):
+        """
+        Make the zombie wander randomly on the map
+        """
+        self.move(randint(-1, 1), randint(-1, 1))
+        
+    def chase(self, player):
+        """
+        Make the zombie chase a player
+        """
+        pass
