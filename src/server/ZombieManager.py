32b0db29d401fd67f4d081976d6adda1211ad250
Alexandre ANGOT <alexandre.angot21@orange.fr>
2023-12-07 23:26:19 +0100

    🚚 separate code in distinct methods

--- src/server/ZombieManager.py
+++ src/server/ZombieManager.py
@@ -1,31 +1,31 @@
 from Zombie import Zombie
 import threading
+import j2l.pytactx.agent as pytactx
+from time import sleep
 
 class ZombieManager:
     
-    def __init__(self, arbitre):
+    def __init__(self, arbitre: pytactx.Agent, zombies_names: list):
         self.zombies = []
+        self.arbitre = arbitre
+        self.zombies_names = zombies_names
 
     def create_zombie(self, name: str):
         """
         Generate a zombie instance
         """
         zombie = Zombie(name)
+        zombie.update()
         
-        while True:
-            zombie.wander()
-            
-            # TODO: Add here the behavior of the zombie
-            
-            zombie.update()
+        self.manage_state(zombie)
 
-    def create_zombies(self, zombies_names: list):
+    def create_zombies(self):
         """
         Generate a list of zombies instances from a list of names
         """
         
         # Create a thread for each zombie
-        for name in zombies_names:
+        for name in self.zombies_names:
             print("Création du thread", name)
             thread = threading.Thread(target=self.create_zombie, args=(name,))
             self.zombies.append(thread)
@@ -36,3 +36,15 @@ class ZombieManager:
             thread.join()
             
         print("Tous les threads ont terminé.")
+
+    def manage_state(self, zombie: Zombie):
+        """
+        Manage the state (wander or chase) of the zombies
+        """
+        while True:
+            if not zombie.is_alive():
+                print("Le zombie", zombie.playerId, "est mort.")
+                break
+            
+            zombie.wander()
+            zombie.update()
