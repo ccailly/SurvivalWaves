1cc4416dd0fcf13f22966079e472cd94057ecc82
Alexandre ANGOT <alexandre.angot21@orange.fr>
2024-01-12 10:39:23 +0100

    ✨ add Zombie interface

---
+++ src/server/IZombie.py
@@ -0,0 +1,16 @@
+import j2l.pytactx.agent as pytactx
+from random import randint
+
+class IZombie (pytactx.Agent):
+
+    def wander(self):
+        """
+        Make the zombie wander randomly on the map
+        """
+        pass
+        
+    def chase(self, player):
+        """
+        Make the zombie chase a player
+        """
+        pass
