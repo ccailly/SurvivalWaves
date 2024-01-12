9a14bd71aaa46099a88bed0177e6f371783ba2d2
Alexandre ANGOT <alexandre.angot21@orange.fr>
2024-01-12 10:36:00 +0100

    ✨ add player_close method on Zombie

--- src/server/Zombie.py
+++ src/server/Zombie.py
@@ -2,7 +2,7 @@ import j2l.pytactx.agent as pytactx
 from random import randint
 
 class Zombie (pytactx.Agent):
-    
+
     def __init__(self, name: str) -> None:
         super().__init__(
             playerId=name,
@@ -18,7 +18,7 @@ class Zombie (pytactx.Agent):
         Make the zombie wander randomly on the map
         """
         self.move(randint(-1, 1), randint(-1, 1))
-        
+
     def chase(self, x: int, y: int) -> None:
         """
         Make the zombie chase a player
@@ -30,9 +30,11 @@ class Zombie (pytactx.Agent):
         Check if the zombie is alive
         """
         return self.life > 0
-    
+
     def player_close(self) -> (int, int):
         """
         Check if a player is close to the zombie
         """
-        pass
+        for name, attributs in self.range.items():
+            return (attributs['x'], attributs['y'])
+        return None
