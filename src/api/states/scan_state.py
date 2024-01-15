37cf1a37e9ea52ce8d17a7d7e87f820d67133c28
HelieTo <116814196+HelieTo@users.noreply.github.com>
2024-01-15 20:04:59 +0000

    mouvements aléatoires joueur

--- src/api/states/scan_state.py
+++ src/api/states/scan_state.py
@@ -1,5 +1,5 @@
 from states.abstract_state import *
-
+import random
 
 class ScanState(AbstractState):
 
@@ -13,33 +13,9 @@ class ScanState(AbstractState):
 
     def process(self) -> None:
         if self.checks() == False:
-            return
-        point_TL = [9, 8]  
-        point_TR = [25, 8]
-        point_BR = [25, 25]
-        point_BL = [9, 25]
-        super().process()
-
-        # Check if the agent is at the position
-        # if (
-        #     self.agent.x == self.instance.positions[0][0]
-        #     and self.agent.y == self.instance.positions[0][1]
-        # ):
-        #     self.instance.positions.append(self.instance.positions.pop(0))
-        #     return
-
-        if self.agent.x == point_TL[0] and self.agent.y == point_TL[1]:
-            self.agent.moveTowards(point_TR[0], point_TR[1])
-        elif self.agent.x == point_TR[0] and self.agent.y == point_TR[1]:
-            self.agent.moveTowards(point_BR[0], point_BR[1])
-        elif self.agent.x == point_BR[0] and self.agent.y == point_BR[1]:
-            self.agent.moveTowards(point_BL[0], point_BL[1])
-        elif self.agent.x == point_BL[0] and self.agent.y == point_BL[1]:
-            self.agent.moveTowards(point_TL[0], point_TL[1])    
-        else :
-            self.agent.moveTowards(point_TL[0], point_TL[1])
+            return 
 
         self.agent.moveTowards(
-            self.instance.positions[0][0], self.instance.positions[0][1]
+            random.randint(2, 33) , random.randint(2,34)
         )
         self.agent.update()
